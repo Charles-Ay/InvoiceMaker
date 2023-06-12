91ac6f8d94b7a3f1bb05336b749eab552628dc9b
Charles Ayeni <charles.work20@gmail.com>
2023-06-12 11:50:39 -0400

    Added startup

--- Back/InvoiceMaker/Program.cs
+++ Back/InvoiceMaker/Program.cs
@@ -50,37 +50,5 @@ namespace InvoiceMaker
 
             app.Run();
         }
-
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddCors(options =>
-            {
-                options.AddPolicy("CorsPolicy",
-                    builder => builder
-                    .AllowAnyOrigin()
-                    .AllowAnyMethod()
-                    .AllowAnyHeader());
-            });
-
-            services.AddControllers();
-        }
-
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseRouting();
-
-            app.UseCors("CorsPolicy"); // Make sure this line comes before UseEndpoints()
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
-
     }
 }
---
+++ Back/InvoiceMaker/Startup.cs
@@ -0,0 +1,36 @@
+﻿namespace InvoiceMaker
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder
+                    .AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
+
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseCors("CorsPolicy"); // Make sure this line comes before UseEndpoints()
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
